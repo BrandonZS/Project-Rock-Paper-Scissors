d62b7cdc1dee74d639ff3dcb3f773bae07bf7ac7
brandonLinux <abrandon34zs@gmail.com>
2023-10-22 19:27:32 -0600

    Created getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let randNumber= Math.floor(Math.random() * (3) ) + 1;
+    const decision = {
+        1 : "Rock",
+        2 : "Paper",
+        3 : "Scissors"
+    };
+    return decision[randNumber];
+    
+}
+
+console.log(getComputerChoice());
