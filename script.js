2c123e47c68203f3db03c83137839f23538a77a2
brandonLinux <abrandon34zs@gmail.com>
2023-10-22 20:27:00 -0600

    Created playRound() and game()

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 function getComputerChoice() {
     let randNumber= Math.floor(Math.random() * (3) ) + 1;
     const decision = {
-        1 : "Rock",
-        2 : "Paper",
-        3 : "Scissors"
+        1 : "rock",
+        2 : "paper",
+        3 : "scissors"
     };
     return decision[randNumber];
-    
 }
 
-console.log(getComputerChoice());
+function playRound(compChoice,userChoice) {
+    compChoice = getComputerChoice();
+    userChoice = (prompt("Rock, Paper or Scissors?")).toLowerCase();
+
+    if (compChoice!=userChoice) {
+
+        if (compChoice=="rock"&&userChoice=="scissors" || compChoice=="paper"&&userChoice=="rock" || compChoice=="scissors"&&userChoice=="paper"){
+            return `You lose! ${compChoice} beats ${userChoice}`
+            
+        } else{
+            return `You win! ${userChoice} beats ${compChoice}`
+        }
+    } else {
+        return "Draw!"
+    }
+}
+
+function game(result) {
+    let userCounter = 0;
+    let compCounter = 0;
+    for (let i = 0; i < 5; i++) {
+        result = playRound()
+        console.log(result);
+        result = result.substring(0,7)
+        if (result == "You los") {
+            compCounter++;
+        } else if(result == "You win") {
+            userCounter++;
+        }  
+    }
+    
+    return `Final score --> User: ${userCounter} Computer: ${compCounter}`
+}
+console.log(game());
